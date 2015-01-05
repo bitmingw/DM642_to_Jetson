baf3c8c4d8d8b5f011b4924991a4d1143a0f2165
Ming Wen <bitmingw@gmail.com>
2015-01-05 17:17:13 +0800

    3 difference frames display. unknown bug that first frame always exist.

--- main.hpp
+++ main.hpp
@@ -9,4 +9,22 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-int three_diff_frame(cv::VideoCapture * stream_ptr, int delay_ms);
+// Processing algorithm
+int three_diff_frame(cv::VideoCapture *stream_ptr, int delay_ms);
+
+// Helper: determine the buffer to store the next frame
+int get_next_frame_position(int current_position);
+
+// Helper: read 1 gray frame
+int read_1_gray_frame(cv::VideoCapture *stream_ptr, cv::Mat *frame);
+
+// Helper: read 3 gray frames
+int read_3_gray_frames(cv::VideoCapture *stream_ptr, 
+cv::Mat *frame1, cv::Mat *frame2, cv::Mat *frame3);
+
+// Helper: generate 2 gray difference frames
+int gen_2_gray_diff_frames(int start_position, cv::Mat *diff1, cv::Mat *diff2, 
+cv::Mat *frame1, cv::Mat *frame2, cv::Mat *frame3);
+
+// Helper: combine 2 difference frames to the result
+int combine_diff_frames(cv::Mat *result, cv::Mat *diff1, cv::Mat *diff2);
--- process.cpp
+++ process.cpp
@@ -6,21 +6,51 @@
 using namespace cv;
 using namespace std;
 
-int three_diff_frame(VideoCapture * stream_ptr, int delay_ms) {
-	Mat frame; // frame to be displayed
+int three_diff_frame(VideoCapture *stream_ptr, int delay_ms) {
+	Mat frame_disp; 
+	Mat frame1, frame2, frame3;	// frames to store original images
+	Mat diff_frame1, diff_frame2;	// frames of difference
 	namedWindow("camera");
+
+	int cur_frame_position = 3;	// The position of last frame
+	int frame_read_status = true;	// if next frame exists
+
+	// on the first run, read 3 frames and calculate differences
+	read_3_gray_frames(stream_ptr, &frame1, &frame2, &frame3);
+	gen_2_gray_diff_frames(cur_frame_position, &diff_frame1, &diff_frame2, 
+		&frame1, &frame2, &frame3);
+	combine_diff_frames(&frame_disp, &diff_frame1, &diff_frame2);
+	imshow("camera", frame_disp);
 
 	bool stop = false;
 	while (!stop) {
-		// if there is no next frame, stop the process
-		if (!stream_ptr->read(frame)) {
+
+		// in the loop, read 1 frame and calculate differences
+		cur_frame_position = get_next_frame_position(cur_frame_position);
+
+		switch (cur_frame_position) {
+			case 1:
+				frame_read_status = read_1_gray_frame(stream_ptr, &frame1);
+				break;
+			case 2:
+				frame_read_status = read_1_gray_frame(stream_ptr, &frame2);
+				break;	
+			case 3:
+				frame_read_status = read_1_gray_frame(stream_ptr, &frame3);
+				break;
+		}
+
+		// break when reach the end of file
+		if (!frame_read_status) {
 			break;
 		}
-		
-		// convert the frame into Gray
-		cvtColor(frame, frame, CV_BGR2GRAY);
 
-		imshow("camera", frame);
+		// calculate differences
+		gen_2_gray_diff_frames(cur_frame_position, &diff_frame1, &diff_frame2, 
+			&frame1, &frame2, &frame3);
+		combine_diff_frames(&frame_disp, &diff_frame1, &diff_frame2);
+
+		imshow("camera", frame_disp);
 
 		// delay for next frame
 		// and press any key to stop
@@ -30,3 +60,55 @@ int three_diff_frame(VideoCapture * stream_ptr, int delay_ms) {
 	}
 	return 0;
 }
+
+int get_next_frame_position(int current_position) {
+	if (current_position == 1 || current_position == 2) {
+		return current_position++;
+	} else {
+		return 1;
+	}
+}
+
+int read_1_gray_frame(VideoCapture *stream_ptr, Mat *frame) {
+	if (!stream_ptr->read(*frame)) {
+		return false;
+	}
+	cvtColor(*frame, *frame, CV_BGR2GRAY);
+	return true;
+}
+
+int read_3_gray_frames(VideoCapture *stream_ptr, 
+Mat *frame1, Mat *frame2, Mat *frame3) {
+	stream_ptr->read(*frame1);
+	stream_ptr->read(*frame2);
+	stream_ptr->read(*frame3);
+	cvtColor(*frame1, *frame1, CV_BGR2GRAY);
+	cvtColor(*frame2, *frame2, CV_BGR2GRAY);
+	cvtColor(*frame3, *frame3, CV_BGR2GRAY);
+	return 0;
+}
+
+int gen_2_gray_diff_frames(int start_position, Mat *diff1, Mat *diff2, 
+Mat *frame1, Mat *frame2, Mat *frame3) {
+	switch (start_position) {
+		case 3:
+			(*diff1) = (*frame2) - (*frame1);
+			(*diff2) = (*frame3) - (*frame2);
+			break;
+		case 1:
+			(*diff1) = (*frame3) - (*frame2);
+			(*diff2) = (*frame1) - (*frame3);
+			break;
+		case 2:
+			(*diff1) = (*frame1) - (*frame3);
+			(*diff2) = (*frame2) - (*frame1);
+			break;
+	}
+	return 0;
+}
+
+int combine_diff_frames(Mat *result, Mat *diff1, Mat *diff2) {
+	(*result) = (*diff2) + (*diff1);
+	return 0;
+}
+
