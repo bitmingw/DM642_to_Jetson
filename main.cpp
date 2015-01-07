e3251ebfd296eb5df456ca6c0dc72ebffcdecabd
Ming Wen <bitmingw@gmail.com>
2015-01-07 20:22:52 +0800

    add video writer in main function

--- main.cpp
+++ main.cpp
@@ -3,35 +3,56 @@
 
 #include "main.hpp"
 
+#define IN_STREAM_NAME "camera2.avi"
+#define OUT_STREAM_NAME "disp2.avi"
+
 using namespace cv;
 using namespace std;
 
 int main()
 {
-	string stream_name = "camera2.avi";
-	
-	cv::VideoCapture stream(stream_name);
-	if (!stream.isOpened()) {
-		cout << "Fatal error: can\'t open video stream.\n" << endl;
+	// open input video stream
+	cv::VideoCapture in_stream(IN_STREAM_NAME);
+	if (!in_stream.isOpened()) {
+		cout << "Fatal error: can\'t read from video stream." << endl;
 		return 1;
 	}
 
+	// get the frame size
+	int frame_w = static_cast<int>(in_stream.get(CV_CAP_PROP_FRAME_WIDTH));
+	int frame_h = static_cast<int>(in_stream.get(CV_CAP_PROP_FRAME_HEIGHT));
+	cv::Size frame_size = Size(frame_w, frame_h);
+	int fourcc = CV_FOURCC('X', 'V', 'I', 'D');
+
 	// get the frame rate
-	double frame_rate = stream.get(CV_CAP_PROP_FPS);
-	// cout << "FPS: " << frame_rate << endl;
+	double frame_rate = in_stream.get(CV_CAP_PROP_FPS);
 	if (isnan(frame_rate)) {
-		frame_rate = 30;
+		frame_rate = 25;
 	}
 	cout << "FPS: " << frame_rate << endl;
+
+
+	// open output video stream, if necessary
+#ifdef OUT_STREAM_NAME
+	cv::VideoWriter out_stream(OUT_STREAM_NAME, fourcc, 
+		frame_rate, frame_size); 
+	if (!out_stream.isOpened()) {
+		cout << "Fatal error: can\'t create write stream." << endl;
+		return 1;
+	}
+#else
+	cv::VideoWriter out_stream();
+#endif
 
 	// delay between each frame in ms
 	int delay = 1000 / frame_rate;
 
 	// call the algorithm to process and display the video
-	three_diff_frame(&stream, delay);
+	three_diff_frame(&in_stream, delay);
 
-	// close the stream
-	stream.release();
+	// close the streams
+	in_stream.release();
+	out_stream.release();
 
 	return 0;
 }
